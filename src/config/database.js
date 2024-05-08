469afb36bddb5ab9bcd01cefc6af1e2b73ca61bf
santanapenha <maria.santanapenha2006@gmail.com>
2024-05-08 16:52:09 -0300

    commit

---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+
+const con = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mysql",
+    database: "food"
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Conectado ao banco de dados");
+});
+
+
+export {con};
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import {con} from "./config/database.js";
+
 
 const app = express();
 
@@ -10,6 +12,16 @@ app.use(cors());
 app.get("/olamundo", function(req, res){
     res.status(200).send("Olá Mundo! Eu me chama servidor node.js");
 });
+
+app.get("/produtos", function(rec, res){
+    con.query('select * from produto', function(err, result){
+        if (err)
+            return res.status(500).send("Ocorreu um erro: " + err.message);
+        else
+            return res.status(200).json(result);
+    })
+});
+
 
 //Levantar o servidor 
 app.listen(3001, function(){
