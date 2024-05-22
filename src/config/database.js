4e568c761e6dee5640d0ec9925cb2a060beb86f4
santanapenha <maria.santanapenha2006@gmail.com>
2024-05-22 10:37:16 -0300

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
+import {} from "dotenv/config.js";
 import mysql from "mysql2";
 
 const con = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mysql",
-    database: "food"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 con.connect(function(err) {
